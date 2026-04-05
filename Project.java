9614953d50cbd8e13a5bd5726cf0f179a6d1a60a
deekshithamalla-ship-it <deekshitha.malla@gmail.com>
2026-04-05 20:50:59 +0530

    Project.java

--- Project.java
+++ Project.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class project1 {
+public class project {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.print("enter the number of jobs");
